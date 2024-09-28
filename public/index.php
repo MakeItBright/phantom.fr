6a988b96a3019e18a4bbf68e0f90d88f0d520672
j.breslauer <yb.apple.dev@gmail.com>
2024-09-29 00:12:22 +0200

    upd: add config

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-phpinfo();
+$start_framework = microtime(true);
+
+
+if (PHP_MAJOR_VERSION < 8) {
+    die('Require PHP version >= 8');
+}
+
+
+var_dump("Time: " . microtime(true) - $start_framework);
